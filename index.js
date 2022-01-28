246b824cb1d2ed190a5ffa28824d9c1d1ebbb6ee
unknown <jam319@protonmail.com>
2022-01-27 19:20:07 -0500

    changed doggos according to exercise

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = 'left'
 
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++){
+    dogImages[i].style.borderRadius = '50px'
+}
+
+let dogNames = document.querySelectorAll('.dog-name')
+for(let i = 0; i <dogImages.length; i++){
+    dogNames[i].style.textAlign = 'left'
+}
+
+let footer = document.querySelector('.footer')
+footer.style.color = 'hotpink'
+footer.style.backgroundColor = 'black'
 // Select some elements...
